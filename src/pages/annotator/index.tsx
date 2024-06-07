8de84b4caa87709551faacd169018e57a27e0000
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2024-06-07 22:01:54 +1200

    🚧 Start filling out Annotator

--- src/pages/annotator/index.tsx
+++ src/pages/annotator/index.tsx
@@ -1,6 +1,25 @@
+function runQuery() {
+    
+}
+
+
 function Annotator() {
     return (
-        <>ANNOTATOR</>
+        <main>
+            <h1>Annotator</h1>
+            <div>
+                <label htmlFor="query">Observation query options:</label>
+                <input type="text" id="query" name="query" size={100}/>
+                <button type="button" id="queryButton" name="queryButton" onClick={runQuery}>Run Query</button>
+            </div>
+            TODO
+            <ul>
+                <li>Enter filter query string to start with (no ui yet to select all filters)</li>
+                <li>Select "without annotation" fields - ensure resulting query string includes this</li>
+                <li>Load the first x obs and display them in small row based? UI</li>
+                <li>allow select annotation - requires AUTH</li>
+            </ul>
+        </main>
     );
 }
 

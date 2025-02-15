fe04b5ddf7997f23e88f1cc5f6271246f182dd61
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2025-02-15 21:00:22 +1300

    🐛 Fix images not dispalying in Firefox

--- src/components/observation-hero/ObservationHero.tsx
+++ src/components/observation-hero/ObservationHero.tsx
@@ -26,7 +26,7 @@ const ObservationHero: React.FC<ObservationHeroProps> = ({ observation, site }) 
             {photos.map(photo => (
               <Carousel.Slide key={photo.id}>
                 <Center h='50vh'>
-                  <Image src={getPhotoUrl(photo, "medium")}/>
+                  <Image src={getPhotoUrl(photo, "medium")} style={{height: '50vh', width:'50vh'}}/>
                 </Center>
               </Carousel.Slide>
             ))}

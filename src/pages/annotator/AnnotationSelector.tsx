4f81f5d526ee701a56ee68bd80e5d4fee47602ba
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2025-02-27 21:55:31 +1300

    🐛 Fix multi-annotated annotations not displaying

--- src/pages/annotator/AnnotationSelector.tsx
+++ src/pages/annotator/AnnotationSelector.tsx
@@ -10,31 +10,36 @@ interface AnnotationSelectorProps {
 export function AnnotationSelector({observation, observationControlledTerms, currentUser}: AnnotationSelectorProps ) {
     return(<Stack gap="md">
         {observationControlledTerms.map(controlledTerm => {
-            const annotation = observation.annotations.find(a => a.controlled_attribute_id === controlledTerm.id);
+            const annotations = observation.annotations.filter(a => a.controlled_attribute_id === controlledTerm.id);
+            const othersAnnotations = annotations.filter(a => a.user_id !== currentUser?.id);
+            const yourAnnotations = annotations.filter(a => a.user_id === currentUser?.id);
             return(
                 <Stack gap="xs" key={controlledTerm.id}>
                     <Title size="md">{controlledTerm.label}</Title>
-                    {annotation && annotation.user_id !== currentUser?.id ?
+                    <Group gap="xs">
+                    {othersAnnotations.map(annotation => 
                         <Group gap="xs">
                             <Chip checked={true} variant="light">{controlledTerm.values.find(t => t.id == annotation.controlled_value_id)?.label}</Chip>
                             <UserAccount size="sm" user={annotation.user} />
                         </Group>
-                    : 
+                    )}
+                    {othersAnnotations.length == 0 || (controlledTerm.multivalued && controlledTerm.values.length > othersAnnotations.length) ? 
                         <Chip.Group multiple={controlledTerm.multivalued}>
                             <Group gap="xs">
-                            {controlledTerm.values.map(controlledTermValue => {
+                            {controlledTerm.values.filter(x => othersAnnotations.every(y => y.controlled_value_id != x.id)).map(controlledTermValue => {
                                 return(
                                     <Chip 
                                         key={controlledTermValue.id}
                                         value={controlledTermValue.id} 
-                                        checked={annotation?.controlled_value_id === controlledTermValue.id}>
+                                        checked={yourAnnotations.some(annotation => annotation?.controlled_value_id === controlledTermValue.id)}>
                                         {controlledTermValue.label}
                                     </Chip>
                                 );
                             })}
                             </Group>
                         </Chip.Group>
-                    }
+                    : null}
+                    </Group>
                 </Stack>
             );
         })}

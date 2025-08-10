c5ca89e09ef8902a79829cf28dbb92cb85cdd67a
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2025-08-10 12:25:43 +1200

    🔧 Attempt to fix GH pages routing

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,30 +1,34 @@
 import Home from "./pages/home";
 import SiteSelectionPage from "./pages/site-selection";
 import Header from "./components/header";
-import { Route } from "wouter";
+import { Route, Router } from "wouter";
 import useSite from "./hooks/useSite";
 import useAuthentication from "./hooks/useAuthentication";
 import Annotator from "./pages/annotator";
 import { AuthContext } from "./Contexts";
+import OauthRedirect from "./pages/oauth-redirect";
 
 function AppContent() {
 	const [site, setSite] = useSite();
 	const [authentication] = useAuthentication(site);
 	return (
 		<AuthContext value={authentication}>
-			<Route path="/">
-				<Header site={site} />
-				<Home />
-			</Route>
-			<Route path="/site-selection">
-				<Header site={site} />
-				<SiteSelectionPage site={site} setSite={setSite} />
-			</Route>
-			<Route path="/annotator">
-				<Annotator site={site} authentication={authentication} />
-			</Route>
-			<Route path="/oauth-redirect">
-			</Route>
+			<Router base={import.meta.env.BASE_URL}>
+				<Route path="/">
+					<Header site={site} />
+					<Home />
+				</Route>
+				<Route path="/site-selection">
+					<Header site={site} />
+					<SiteSelectionPage site={site} setSite={setSite} />
+				</Route>
+				<Route path="/annotator">
+					<Annotator site={site} authentication={authentication} />
+				</Route>
+				<Route path="/oauth-redirect">
+					<OauthRedirect/>
+				</Route>
+			</Router>
 		</AuthContext>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: "/inaturalist-tools/",
-	plugins: [react()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		base: env.VITE_BASE_PATH,
+		plugins: [react()],
+	};
 });

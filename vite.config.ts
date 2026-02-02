909cf2938f592032b2eb6ff5ad9505d8b073dba2
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2026-02-02 20:29:18 +1300

    🔒️ Add Content Security Policy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,5 +7,12 @@ export default defineConfig(({ mode }) => {
 	return {
 		base: env.VITE_BASE_PATH,
 		plugins: [react()],
+		host: process.env.VITE_SERVER_HOST,
+		preview: {
+			headers: {
+				"content-security-policy":
+					"default-src 'self'; style-src-elem 'self' 'unsafe-inline'; connect-src https://api.inaturalist.org; img-src 'self' *.inaturalist.org https://inaturalist-open-data.s3.amazonaws.com",
+			},
+		},
 	};
 });

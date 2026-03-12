26983e37f773efaa57e181d2491bf8cf89d07a7d
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2026-03-12 18:52:57 +1300

    💄 Tweak Annotations display

--- src/pages/annotator/AnnotationSelector.tsx
+++ src/pages/annotator/AnnotationSelector.tsx
@@ -17,7 +17,7 @@ export function AnnotationSelector({
 }: AnnotationSelectorProps) {
 	const currentUser = useContext(CurrentUserContext);
 	return (
-		<Stack gap="md" pt={3} pl={3}>
+		<Stack gap="lg" pt={18} pl={3}>
 			{observationControlledTerms.map((controlledTerm) => {
 				const annotations = observation.annotations.filter(
 					(a) => a.controlled_attribute_id === controlledTerm.id,
@@ -29,34 +29,32 @@ export function AnnotationSelector({
 					(a) => a.user_id === currentUser?.id,
 				);
 				return (
-					<Stack gap="xs" key={controlledTerm.id}>
+					<Group gap="xs" key={controlledTerm.id}>
 						<Title size="md">{controlledTerm.label}</Title>
-						<Group gap="xs">
-							{othersAnnotations.map((annotation) => (
-								<Group gap="xs" key={annotation.uuid}>
-									<Chip checked={true} variant="light">
-										{
-											controlledTerm.values.find(
-												(t) => t.id === annotation.controlled_value_id,
-											)?.label
-										}
-									</Chip>
-									<UserAccount size="sm" user={annotation.user} />
-								</Group>
-							))}
-							{othersAnnotations.length === 0 ||
-							(controlledTerm.multivalued &&
-								controlledTerm.values.length > othersAnnotations.length) ? (
-								<EditableChipGroup
-									observation={observation}
-									controlledTerm={controlledTerm}
-									yourAnnotations={yourAnnotations}
-									othersAnnotations={othersAnnotations}
-									annotationFunctions={annotationFunctions}
-								/>
-							) : null}
-						</Group>
-					</Stack>
+						{othersAnnotations.map((annotation) => (
+							<Group gap="xs" key={annotation.uuid}>
+								<Chip checked={true} variant="light">
+									{
+										controlledTerm.values.find(
+											(t) => t.id === annotation.controlled_value_id,
+										)?.label
+									}
+								</Chip>
+								<UserAccount size="sm" user={annotation.user} />
+							</Group>
+						))}
+						{othersAnnotations.length === 0 ||
+						(controlledTerm.multivalued &&
+							controlledTerm.values.length > othersAnnotations.length) ? (
+							<EditableChipGroup
+								observation={observation}
+								controlledTerm={controlledTerm}
+								yourAnnotations={yourAnnotations}
+								othersAnnotations={othersAnnotations}
+								annotationFunctions={annotationFunctions}
+							/>
+						) : null}
+					</Group>
 				);
 			})}
 		</Stack>

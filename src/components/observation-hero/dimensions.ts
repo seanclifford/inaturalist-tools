cf8c0efef94fea9e379b766580138268ca53d82c
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2026-03-20 21:52:35 +1300

    💄 Reduce photo banner height

--- src/components/observation-hero/dimensions.ts
+++ src/components/observation-hero/dimensions.ts
@@ -1,7 +1,7 @@
 export const outerHeight = "min(50vh, 100vw)";
 export const outerWidth = "min(50vh, 100vw)";
 
-export const photoBannerHeight = "5em";
+export const photoBannerHeight = "4.1em";
 
 export function getMainImageHeight(photoCount: number) {
 	const subtractAmount = photoCount > 1 ? ` - ${photoBannerHeight}` : "";

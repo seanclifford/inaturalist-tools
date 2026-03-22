a43bc96216db9664b4d479f44979afe16366a551
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2026-03-22 21:03:27 +1300

    💬 Update Help section

--- src/components/settings/HelpSection.tsx
+++ src/components/settings/HelpSection.tsx
@@ -5,7 +5,7 @@ export default function HelpSection() {
 		<>
 			<Title order={3}>Welcome to the mobile fast annotator!</Title>
 			<Text>
-				I created this to allow fast annotating of observations on{" "}
+				This was created to allow fast annotating of observations on{" "}
 				<Anchor href="https://www.inaturalist.org">iNaturalist</Anchor> through
 				a mobile device. <br />
 				Before you can start annotating, you need to
@@ -37,6 +37,33 @@ export default function HelpSection() {
 					supported by iNaturalist
 				</Anchor>
 				.
+			</Text>
+			<br />
+			<Title order={4}>Choosing an annotation</Title>
+			<Text>
+				Annotations have specific meanings. If you're unsure of which annotation
+				values are appropriate for each situation, please refer to{" "}
+				<Anchor
+					href="https://help.inaturalist.org/en/support/solutions/articles/151000191830-what-are-the-definitions-of-inaturalist-annotations-"
+					target="_blank"
+				>
+					this iNaturalist help article
+				</Anchor>
+				.
+			</Text>
+			<br />
+			<Title order={4}>Annotating on a laptop or desktop computer</Title>
+			<Text>
+				This site has been made for mobile and tablets in mind. While it will
+				work on laptops and desktop computers, you may find using the Identify
+				page to be a better experience. Read{" "}
+				<Anchor
+					href="https://www.inaturalist.org/pages/annotations"
+					target="_blank"
+				>
+					this iNaturalist page on annotations
+				</Anchor>{" "}
+				if you're new to annotating using the Identify page.
 			</Text>
 		</>
 	);

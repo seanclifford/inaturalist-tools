04a118d459f6c5d46b50cb81ae0ec6fe32847ece
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2026-03-31 20:36:43 +1300

    ✨ Add adding other unsupported filters

--- src/components/observation-filter/OtherFilters.tsx
+++ src/components/observation-filter/OtherFilters.tsx
@@ -1,4 +1,12 @@
-import { Group, Pill } from "@mantine/core";
+import {
+	Button,
+	Group,
+	Pill,
+	Stack,
+	TextInput,
+	useMantineTheme,
+} from "@mantine/core";
+import { useRef } from "react";
 import { observationParamsWithoutFields } from "../../inaturalist/constants";
 
 interface OtherFiltersParams {
@@ -10,23 +18,51 @@ export function OtherFilters({
 	searchParams,
 	onParamChange,
 }: OtherFiltersParams) {
+	const theme = useMantineTheme();
+	const newKeyRef = useRef<HTMLInputElement>(null);
+	const newValRef = useRef<HTMLInputElement>(null);
+
 	const otherParams = [...getOtherParams(searchParams)];
+
+	const onAdd = () => {
+		if (!newKeyRef.current || !newValRef.current) return;
+		const key = newKeyRef.current.value;
+		const val = newValRef.current.value;
+		newKeyRef.current.value = newValRef.current.value = "";
+		onParamChange(key, val);
+	};
+
 	const onRemove = (key: string) => onParamChange(key, null);
+
 	return (
-		<Group>
-			{otherParams.map((param) => {
-				const pillValue = `${param[0]}=${param[1]}`;
-				return (
-					<Pill
-						key={pillValue}
-						withRemoveButton
-						onRemove={() => onRemove(param[0])}
-					>
-						{pillValue}
-					</Pill>
-				);
-			})}
-		</Group>
+		<Stack>
+			<Group align="end" gap="sm">
+				<TextInput
+					ref={newKeyRef}
+					label="Other Filters"
+					placeholder="Filter Key"
+				/>
+				<TextInput ref={newValRef} placeholder="Filter Value" />
+				<Button onClick={onAdd} color={theme.colors.gray[6]}>
+					Add
+				</Button>
+			</Group>
+			<Group pb="sm">
+				{otherParams.map((param) => {
+					const pillValue = `${param[0]}=${param[1]}`;
+					return (
+						<Pill
+							size="md"
+							key={pillValue}
+							withRemoveButton
+							onRemove={() => onRemove(param[0])}
+						>
+							{pillValue}
+						</Pill>
+					);
+				})}
+			</Group>
+		</Stack>
 	);
 }
 
